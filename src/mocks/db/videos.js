e400e6702a8d9b0f28de3c7cf6a7b5969393be64
Prathamesh Mali <mali.prathamesh82@gmail.com>
2022-04-10 01:28:20 +0530

    feat: added mock data

--- src/mocks/db/videos.js
+++ src/mocks/db/videos.js
@@ -5,17 +5,77 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: "zDNaUi2cjv4",
+    alt: "video alt",
+    views: "206573",
+    duration: 135,
+    title: "Binary Explained in 01100100 Seconds",
+    thumbnail: "https://i3.ytimg.com/vi/zDNaUi2cjv4/maxresdefault.jpg",
+    channelName: "Fireship",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLR5CDv14gL4DQ7I4gxIlBMY6u-CNsq2qfeev48R2g=s48-c-k-c0x00ffffff-no-rj",
+    category: "CS 101",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "Binary is a base-2 numbering system and the final language used by computer hardware. Programmers don't write binary code directly, but it's import to understand how a machine uses transistors to organize billions of bits to represent data.",
   },
+
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "Tn6-PIqc4UM",
+    alt: "video alt",
+    views: "612,919",
+    duration: 124,
+    title: "React in 100 Seconds",
+    thumbnail: "https://i3.ytimg.com/vi/Tn6-PIqc4UM/maxresdefault.jpg",
+    channelName: "Fireship",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLR5CDv14gL4DQ7I4gxIlBMY6u-CNsq2qfeev48R2g=s48-c-k-c0x00ffffff-no-rj",
+    category: "React",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "React is a little JavaScript library with a big influence over the webdev world. Learn the basics of React in 100 Seconds",
+  },
+
+  {
+    _id: "ENrzD9HAZK4",
+    alt: "video alt",
+    views: "720,860",
+    duration: 971,
+    title: "Node.js Ultimate Beginner’s Guide in 7 Easy Steps",
+    thumbnail: "https://i3.ytimg.com/vi/ENrzD9HAZK4/maxresdefault.jpg",
+    channelName: "Fireship",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLR5CDv14gL4DQ7I4gxIlBMY6u-CNsq2qfeev48R2g=s48-c-k-c0x00ffffff-no-rj",
+    category: "Backend Development",
+    description:
+      "Why learn Node.js in 2020? Master the fundamentals of Node in 7 easy steps, then build a fullstack web app and deploy it to a cloud server.",
+  },
+
+  {
+    _id: "wHTcrmhskto",
+    alt: "video alt",
+    views: "510,967",
+    duration: 556,
+    title: "Is Web3 all Hype? Top 10 Web 3.0 Questions & Answers",
+    thumbnail: "https://i3.ytimg.com/vi/wHTcrmhskto/maxresdefault.jpg",
+    channelName: "Fireship",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLR5CDv14gL4DQ7I4gxIlBMY6u-CNsq2qfeev48R2g=s48-c-k-c0x00ffffff-no-rj",
+    category: "Web 3",
+    description:
+      "A complete introduction into the wild world of Web3 - the decentralized web. Is Web 3.0 the future or is it all hype? Learn about the technologies and patterns used when building a dapp on the blockchain.",
+  },
+
+  {
+    _id: "ifTF3ags0XI",
+    alt: "video alt",
+    views: "923,731",
+    duration: 680,
+    title: "25 VS Code Productivity Tips and Speed Hacks",
+    thumbnail: "https://i3.ytimg.com/vi/ifTF3ags0XI/maxresdefault.jpg",
+    channelName: "Fireship",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLR5CDv14gL4DQ7I4gxIlBMY6u-CNsq2qfeev48R2g=s48-c-k-c0x00ffffff-no-rj",
+    category: "Developer Productivity",
+    description:
+      "Learn 25 VS Code tips and tricks that will help you write code faster. Try out awesome new features and extensions that turn your editor into a full-blown IDE.",
   },
 ];
