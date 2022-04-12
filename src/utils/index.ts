09f795dd5c964c10a60b4654cb8468621efe22fc
Prathamesh Mali <mali.prathamesh82@gmail.com>
2022-04-12 19:44:37 +0530

    feat: added utility functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,11 @@
 export const getTheme = (isDark: any) => (isDark ? "" : "dark-");
 
+/**
+ *
+ * @param videos: array, of unfiltered videos
+ * @param selectedCategories: string, category to filter
+ * @returns finalVideos: array: filtered categories
+ */
 export const getFilteredVideos = (videos: any, selectedCategories: any) => {
   let finalVideos = videos;
   if (selectedCategories.length > 0) {
@@ -9,3 +15,14 @@ export const getFilteredVideos = (videos: any, selectedCategories: any) => {
   }
   return finalVideos;
 };
+
+/**
+ *
+ * @param targetId :string
+ * @param entity : array
+ * @returns boolean
+ */
+export const isDuplicate = (targetId: any, entity: any) =>
+  entity
+    .map(({ _id: currId }: any) => currId === targetId)
+    .some((val: any) => val);
