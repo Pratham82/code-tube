af2785524121a2aa10d61de5fe0b77feb7aeea95
Prathamesh Mali <mali.prathamesh82@gmail.com>
2022-04-12 19:45:29 +0530

    feat: added services for predefined playlists and types

---
+++ src/types/playlists.ts
@@ -0,0 +1,11 @@
+export const INIT_PLAYLIST = "INITIALIZE_PLAYLIST";
+export const ADD_TO_WATCH_LATER = "Added to watch later";
+export const REMOVE_WATCH_LATER = "Removed from watch later";
+export const ADD_TO_LIKED = "Added to liked videos";
+export const REMOVE_LIKED = "Removed from liked videos";
+export const ADD_TO_PLAYLIST = "Added to playlist";
+export const REMOVE_FROM_PLAYLIST = "Removed from playlist";
+
+export const WATCH_LATER = "WATCH_LATER";
+export const LIKES = "LIKES";
+export const PLAYLIST = "PLAYLIST";
